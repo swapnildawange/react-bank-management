0f9377a7ec43a8977c83f99d958ddce9ec2aeb6a
swapnil <swapnildawange3650@gmail.com>
2022-02-12 23:44:19 +0530

    testing saga

--- src/redux/sagas/__test__/usersSaga.test.js
+++ src/redux/sagas/__test__/usersSaga.test.js
@@ -1,15 +1,6 @@
-import { GET_USERS_INITIATE } from "../../users/action.types";
 import { handleGetUsers } from "../handlers/users";
 
 jest.mock("axios");
-
-const userData = {
-  id: 2,
-  email: "janet.weaver@reqres.in",
-  first_name: "Janet",
-  last_name: "Weaver",
-  avatar: "https://reqres.in/img/faces/2-image.jpg",
-};
 
 describe("get users saga", () => {
   const action = {
@@ -21,5 +12,3 @@ describe("get users saga", () => {
     // expect(generator.next().value.payload.args[0]).toBe(GET_USERS_INITIATE);
   });
 });
-
-
